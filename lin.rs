49df02d31d0f16cce4c72d8503f3f03836b99752
Nguyễn Vĩ Khang <68845361+khangvbeauty@users.noreply.github.com>
2023-07-09 15:09:17 +0700

    Create lin.rs

---
+++ lin.rs
@@ -0,0 +1,131 @@
+// Exercise 1
+// Make it compile
+fn exercise1() {
+    // Use as many approaches as you can to make it work
+    let x = String::from("hello, world");
+    let y = x.clone(); // Clone x into y
+    let z = x; // Move ownership of x to z
+}
+
+// Exercise 2
+// Make it compile
+// Don't modify code in exercise2 function!
+fn exercise2() {
+    let s1 = String::from("hello, world");
+    let s2 = take_ownership(s1); // Move ownership of s1 to take_ownership function
+
+    println!("{}", s2);
+}
+// Only modify the code below!
+fn take_ownership(s: String) -> String { // Return String to give back ownership to the caller
+    println!("{}", s);
+    s // Return String
+}
+
+// Exercise 3
+// Make it compile
+// Dont care about logic
+fn exercise3() {
+    let values: Vec<f64> = vec![
+        2817.42, 2162.17, 3756.57, 2817.42, -2817.42, 946.9, 2817.42, 964.42, 795.43, 3756.57,
+        139.34, 903.58, -3756.57, 939.14, 828.04, 1120.04, 604.03, 3354.74, 2748.06, 1470.8,
+        4695.71, 71.11, 2391.48, 331.29, 1214.69, 863.52, 7810.01,
+    ];
+
+    let values_number = values.len();
+
+    let mut additions: Vec<usize> = vec![0]; // Add mut keyword to allow changing additions
+
+    println!("{:?}", values_number);
+
+    while additions.len() > 0 {
+        let mut addition: f64 = 0.0;
+
+        // Sumar valores en additions
+        for element_index in &additions { // Add & to get references to elements in additions
+            let addition_aux = values[*element_index]; // Add * to dereference element_index
+            addition = addition_aux + addition;
+        }
+        additions.pop(); // Add a statement to remove an element from additions and end the loop
+    }
+}
+
+// Exercise 4
+// Make it compile
+fn exercise4(value: u32) -> String { // Change the return type to String to avoid invalid lifetime of reference
+    let str_value = value.to_string(); // Convert u32 to String
+    str_value // Return String
+}
+
+// Exercise 5
+// Make it compile
+use std::collections::HashMap;
+fn exercise5() {
+    let mut my_map = HashMap::from([(1, "1.0".to_string()), (2, "2.0".to_string())]);
+
+    let key = 3;
+
+    let res = match my_map.get(&key) {
+        Some(child) => child,
+        None => {
+            my_map.entry(key).or_insert("3.0".to_string()); // Use entry and or_insert methods to insert a new value into my_map if it does not exist
+            my_map.get(&key).unwrap() // Get the new value from my_map and unwrap it
+        }
+    };
+
+    println!("{}", res);
+}
+
+// Exercise 6
+// Make it compile
+
+use std::io;
+
+fn exercise6() {
+    let mut prev_key: String = String::new(); // Change the data type to String to be able to assign new values
+
+    for line in io::stdin().lines() {
+        let s = line.unwrap();
+
+        let data: Vec<&str> = s.split("\t").collect();
+        if prev_key.len() == 0 {
+            prev_key = data[0].to_string(); // Convert &str to String using to_string
+        }
+    }
+}
+
+// Exercise 7
+// Make it compile
+fn exercise7() {
+    let mut v: Vec<String> = Vec::new(); // Change the data type to String to be able to store strings created in different scopes
+    {
+        let chars = [b'x', b'y', b'z'];
+        let s: String = std::str::from_utf8(&chars).unwrap().to_string(); // Convert &str to String using to_string
+        v.push(s); // Move ownership of s to v
+    }
+    println!("{:?}", v);
+}
+
+// Exercise 8
+// Make it compile
+fn exercise8() {
+    let mut accounting = vec!["Alice", "Ben"];
+    
+    loop {
+        let mut add_input = String::from("");
+
+        io::stdin()
+            .read_line(&mut add_input)
+            .expect("Failed to read line");
+
+        let add_vec: Vec<&str> = add_input.trim()[..].split_whitespace().collect();
+
+        if add_vec.len() < 1 {
+            println!("Incorrect input, try again");
+            continue;
+        }
+
+        let person = add_vec[0].to_string(); // Convert &str to String using to_string
+        accounting.push(person); // Move ownership of person to accounting
+    }
+}
